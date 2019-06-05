eeb5e34ece1418394577c4179579d00daa0bf4b3
marmii <miriam26@byui.edu>
2019-06-05 16:58:27 -0600

    lesson 6 hamburger menu

---
+++ assignments/lesson-7/js/currentdate.js
@@ -0,0 +1,17 @@
+function currentDate() {
+    var mydate = new Date();
+    var year = mydate.getYear();
+        if(year < 1000){
+            year +=1900
+        }
+    var day = mydate.getDay();
+    var month = mydate.getMonth();
+    var daym = mydate.getDate();
+    var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+var today = document.getElementById("today");
+today.textContent = "" +dayarray[day]+ " " +daym+ " " +montharray[month]+ " " +year;
+today.innerText = "" +dayarray[day]+ " " +daym+ " " +montharray[month]+ " " +year;
+}
+currentDate();

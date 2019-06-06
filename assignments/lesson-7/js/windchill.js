f9e818f56fea35eaa08204400866212f570835ce
marmii <miriam26@byui.edu>
2019-06-06 14:59:46 -0600

    windchill fixes

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -2,6 +2,6 @@
 let t = parseInt(document.getElementById('temp').innerHTML);
 let s = parseInt(document.getElementById('windspeed').innerHTML);
 /*Processing: compute wind chill*/
-var f = 35.74 + 0.6215 * t - (35.75 * Math.pow(windspeed, .16)) + (.4275 * t * Math.pow(windspeed, .16));
+var f = 35.74 + 0.6215 * t - (35.75 * Math.pow(s, .16)) + (.4275 * t * Math.pow(s, .16));
 /*Output: wind chill rounded to whole number*/
 document.getElementById('output').innerHTML = "Wind Chill:" + f.toFixed(0) + "&#8457";

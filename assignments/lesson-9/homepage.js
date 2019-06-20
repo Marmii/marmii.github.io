6e8fb8779c19a1d8dd438a6bae3c76e5b12d4c72
marmii <miriam26@byui.edu>
2019-06-19 21:27:28 -0600

    info fixes

--- assignments/lesson-9/homepage.js
+++ assignments/lesson-9/homepage.js
@@ -1,29 +1,31 @@
 var section = document.querySelector('section');
 var requestURL= 'https://byui-cit230.github.io/weather/data/towndata.json';
+
 var request= new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
     var townInfo = request.response;
-    townData(townInfo);
+    showData(townInfo);
 }
 
-function townData(jsonObj) {
+function showData(jsonObj) {
 var info = jsonObj['towns'];
   
 for (var i = 0; i < info.length; i++) {
+var myArticle = document.createElement('article');
 var myH2 = document.createElement('h2');
 var myH3 = document.createElement('h3');
 var myPara1 = document.createElement('p');
 var myPara2 = document.createElement('p');
 var myPara3 = document.createElement('p');
 
-myH2.textContent = towns[i].name;
-myH3.textContent = '' + towns[i].motto;
-myPara1.textContent = 'Year Founded: ' + towns[i].yearFounded;
-myPara2.textContent = 'Current Population: ' + towns[i].currentPopulation;
-myPara3.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
+myH2.textContent = info[i].name;
+myH3.textContent = '' + info[i].motto;
+myPara1.textContent = 'Year Founded: ' + info[i].yearFounded;
+myPara2.textContent = 'Current Population: ' + info[i].currentPopulation;
+myPara3.textContent = 'Average Rainfall: ' + info[i].averageRainfall;
 
 }
 

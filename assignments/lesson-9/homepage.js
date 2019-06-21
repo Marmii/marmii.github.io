9b45cb10c5e6f72344696bff2cb14b3036a5b535
marmii <miriam26@byui.edu>
2019-06-20 22:36:03 -0600

    commit

--- assignments/lesson-9/homepage.js
+++ assignments/lesson-9/homepage.js
@@ -1,6 +1,7 @@
-var cityInfo = document.getElementById("cityinfo");
-var requestURL= "https://marmii.github.io/assignments/lesson-9/towndata.json";
-//var requestURL= "https://byui-cit230.github.io/weather/data/towndata.json";
+var header = document.querySelector("header");
+var section= document.querySelector("section");
+
+var requestURL= "https://byui-cit230.github.io/weather/data/towndata.json";
 
 var request= new XMLHttpRequest();
 request.open("GET", requestURL);
@@ -8,6 +9,7 @@ request.responseType = 'json';
 request.send();
 request.onload = function() {
     var townInfo = request.response;
+    populateHeader(data);
     showData(townInfo);
 }
 
@@ -18,29 +20,24 @@ var currentTown = document.querySelector("name").innerHTML.split("")[0];
 for (var i = 0; i < info.length; i++) {
 
     if(town[i].name == currentTown) {
-
+    var myArticle = document.createElement("article");
     var myH2 = document.createElement('h2');
     var myH3 = document.createElement('h3');
     var myPara1 = document.createElement('p');
     var myPara2 = document.createElement('p');
     var myPara3 = document.createElement('p');
-    var myImage = document.createElement('img');
 
     myH2.textContent = info[i].name;
     myH3.textContent = '' + info[i].motto;
     myPara1.textContent = 'Year Founded: ' + info[i].yearFounded;
     myPara2.textContent = 'Current Population: ' + info[i].currentPopulation;
     myPara3.textContent = 'Average Rainfall: ' + info[i].averageRainfall;
-    myImage.img.src = "https://marmii.github.io/assignments/lesson-9/preston.jpg";
 
-    cityInfo.appendChild(myH2);
-    cityInfo.appendChild(myH3);
-    cityInfo.appendChild(myPara1);
-    cityInfo.appendChild(myPara2);
-    cityInfo.appendChild(myPara3);
-    cityInfo.appendChild(myImage);
-
-
+    myH2.appendChild(myH2);
+    myH3.appendChild(myH3);
+    myPara1.appendChild(myPara1);
+    myPara2.appendChild(myPara2);
+    myPara3.appendChild(myPara3);
     }
 
 

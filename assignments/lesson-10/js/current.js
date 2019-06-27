d6d72be4eaad021a88c1f9b8e86258a2ce5b82f4
marmii <miriam26@byui.edu>
2019-06-27 15:29:01 -0600

    list

--- assignments/lesson-10/js/current.js
+++ assignments/lesson-10/js/current.js
@@ -7,11 +7,11 @@ weatherRequest.onload = function() {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('temp').innerHTML = weatherData.main.temp;
+    document.getElementById('temp').innerHTML = weatherData.list[0].main.temp;
     document.getElementById('current').innerHTML = weatherData.weather.description;
     document.getElementById('humidity').innerHTML = weatherData.main.humidity;
     document.getElementById('windspeed').innerHTML = weatherData.wind.speed;
-    
+
  let desc = weatherData.weather[0].description;
     let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
     document.getElementById('cc-img').setAttribute('src', icon);

b96b48663fc89dc119baea748a3b4435ef5465a0
marmii <miriam26@byui.edu>
2019-06-27 16:10:53 -0600

    copy

--- assignments/lesson-10/js/current.js
+++ assignments/lesson-10/js/current.js
@@ -12,6 +12,13 @@ weatherRequest.onload = function() {
     document.getElementById('humidity').innerHTML = weatherData.list[0].main.humidity;
     document.getElementById('windspeed').innerHTML = weatherData.list[0].wind.speed;
 
+    /*Input: Get temp and windspeed*/
+let t = parseInt(document.getElementById('temp').innerHTML);
+let s = parseInt(document.getElementById('windspeed').innerHTML);
+/*Processing: compute wind chill*/
+var f = 35.74 + 0.6215 * t - (35.75 * Math.pow(s, .16)) + (.4275 * t * Math.pow(s, .16));
+/*Output: wind chill rounded to whole number*/
+document.getElementById('output').innerHTML = "Wind Chill:" + f.toFixed(0) + "&#8457";
  /*let desc = weatherData.weather[0].description;
     let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
     document.getElementById('cc-img').setAttribute('src', icon);

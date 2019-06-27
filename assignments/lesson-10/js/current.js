3d9e853191967447cbd89250b8fb315182c299a5
marmii <miriam26@byui.edu>
2019-06-27 15:18:56 -0600

    preston

--- assignments/lesson-10/js/current.js
+++ assignments/lesson-10/js/current.js
@@ -7,9 +7,13 @@ weatherRequest.onload = function() {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
-/*    let desc = weatherData.weather[0].description;
+    document.getElementById('temp').innerHTML = weatherData.main.temp;
+    document.getElementById('current').innerHTML = weatherData.weather.description;
+    document.getElementById('humidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('windspeed').innerHTML = weatherData.wind.speed;
+    
+ let desc = weatherData.weather[0].description;
     let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
     document.getElementById('cc-img').setAttribute('src', icon);
-    document.getElementById('cc-img').setAttribute('alt', desc);*/
+    document.getElementById('cc-img').setAttribute('alt', desc);
 }

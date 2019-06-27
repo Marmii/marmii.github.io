379fcf60dddbd9fa575623478df64d4678dd3ffc
marmii <miriam26@byui.edu>
2019-06-27 14:51:43 -0600

    waetherapi

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -1,6 +1,5 @@
 let weatherRequest = new XMLHttpRequest();
-let apiURLstring = 'ttps://api.openweathermap.org/data/2.5/forecast?
-id=5604473&units=imperial&APPID=b6dcbfd399dcaf20477af22ae69192c2';
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=b6dcbfd399dcaf20477af22ae69192c2';
 weatherRequest.open('Get', apiURLstring, true);
 weatherRequest.send();
 
@@ -9,8 +8,8 @@ weatherRequest.onload = function() {
     console.log(weatherData);
 
     document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
-    let desc = weatherData.weather[0].description;
-    let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
+/*    let desc = weatherData.weather[0].description;
+    let icon = "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
     document.getElementById('cc-img').setAttribute('src', icon);
-    document.getElementById('cc-img').setAttribute('alt', desc);
+    document.getElementById('cc-img').setAttribute('alt', desc);*/
 }

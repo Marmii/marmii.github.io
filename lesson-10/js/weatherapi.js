7bd003f3b2d4692dd9e395f3199260b728b87b44
marmii <miriam26@byui.edu>
2019-06-27 11:08:57 -0600

    weatheractivity

---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,15 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=b6dcbfd399dcaf20477af22ae69192c2';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.resonseText);
+    console.log(weatherData);
+
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+    let desc = weatherData.weather[0].description;
+    let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
+    document.getElementById('cc-img').setAttribute('src', icon);
+    document.getElementById('cc-img').setAttribute('alt', desc);
+}

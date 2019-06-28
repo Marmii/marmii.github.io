0161dd3812edfd03ab13dd9dfb62c9cee30e2df6
marmii <miriam26@byui.edu>
2019-06-27 22:20:39 -0600

    commit

--- assignments/lesson-10/js/current.js
+++ assignments/lesson-10/js/current.js
@@ -18,7 +18,7 @@ let s = parseInt(document.getElementById('windspeed').innerHTML);
 /*Processing: compute wind chill*/
 var f = 35.74 + 0.6215 * t - (35.75 * Math.pow(s, .16)) + (.4275 * t * Math.pow(s, .16));
 /*Output: wind chill rounded to whole number*/
-document.getElementById('output').innerHTML = "Wind Chill:" + f.toFixed(0) + "&#8457";
+document.getElementById('output').innerHTML = "Wind Chill: " + f.toFixed(0) + "&#8457";
 
 /*for (var i= 0; i< weatherData.list.length; i++){
     if (weatherData.list.dt_txt.includes = "18:00:00"){

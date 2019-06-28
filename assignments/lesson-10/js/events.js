c976804416be51629252d8c7e0b619e49de78477
marmii <miriam26@byui.edu>
2019-06-28 08:09:33 -0600

    commit

--- assignments/lesson-10/js/events.js
+++ assignments/lesson-10/js/events.js
@@ -27,7 +27,7 @@ for (var i = 0; i < towns.length; i++) {
     }
     myList.textContent = towns[i].events;
     myArticle.appendChild(myList);
-    townevents.appendChild(myArticle);
+    townEvents.appendChild(myArticle);
     //document.getElementById('events').innerHTML = info.events;
     /*var townname = document.getElementById(events);
    

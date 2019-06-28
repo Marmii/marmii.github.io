62e3e6977076509f94df9191837cda8047897960
marmii <miriam26@byui.edu>
2019-06-28 14:28:20 -0600

    commit

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -13,7 +13,7 @@ forecastRequest.onload = function() {
     document.getElementById('day4').innerHTML = weatherData.list[31].main.temp;
     document.getElementById('day5').innerHTML = weatherData.list[39].main.temp;
 
-    var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[7].weather.icon + ".png";
+    var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[7].weather[0].icon + ".png";
     var desc = weatherData.list[7].weather.description;
     document.getElementById('weatherimage').scroll = icon;
     

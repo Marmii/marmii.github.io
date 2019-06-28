7ca866d8feb7995486b84efc9e00df5ea94627ef
marmii <miriam26@byui.edu>
2019-06-28 16:31:25 -0600

    commit

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -7,19 +7,26 @@ forecastRequest.onload = function() {
     var weatherData = JSON.parse(forecastRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('day1').innerHTML = weatherData.list[7].main.temp;
+    /*document.getElementById('day1').innerHTML = weatherData.list[7].main.temp;
     document.getElementById('day2').innerHTML = weatherData.list[14].main.temp;
     document.getElementById('day3').innerHTML = weatherData.list[22].main.temp;
     document.getElementById('day4').innerHTML = weatherData.list[31].main.temp;
-    document.getElementById('day5').innerHTML = weatherData.list[39].main.temp;
+    document.getElementById('day5').innerHTML = weatherData.list[39].main.temp;*/
 
-    var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[7].weather[0].icon + ".png";
-    var desc = weatherData.list[7].weather[0].description;
-    document.getElementById('weatherimage1').scroll = icon;
-    
-    document.getElementById('weatherimage1').setAttribute('src', icon);
-    document.getElementById('weatherimage1').setAttribute('alt', desc);
+    for (var i = 0; i < weatherData.length; i++){
+        if(weatherData.list[i].dt_txt.search('18:00:00') != -1){
+            document.getElementById('day1').innerHTML = weatherData.list[i].main.temp;
 
+            var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[i].weather[0].icon + ".png";
+            var desc = weatherData.list[i].weather[0].description;
+            document.getElementById('weatherimage1').scroll = icon;
+            
+            document.getElementById('weatherimage1').setAttribute('src', icon);
+            document.getElementById('weatherimage1').setAttribute('alt', desc);
+        }
+    }
+   
+/*
     var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[14].weather[0].icon + ".png";
     var desc = weatherData.list[7].weather[0].description;
     document.getElementById('weatherimage2').scroll = icon;
@@ -46,5 +53,5 @@ forecastRequest.onload = function() {
     document.getElementById('weatherimage5').scroll = icon;
     
     document.getElementById('weatherimage5').setAttribute('src', icon);
-    document.getElementById('weatherimage5').setAttribute('alt', desc);
+    document.getElementById('weatherimage5').setAttribute('alt', desc);*/
 }

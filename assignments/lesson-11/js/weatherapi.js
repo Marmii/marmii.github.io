4be59f6607ffde4cfa17f4dbe9278d91898133ef
marmii <miriam26@byui.edu>
2019-07-02 20:01:33 -0600

    table

--- assignments/lesson-11/js/weatherapi.js
+++ assignments/lesson-11/js/weatherapi.js
@@ -1,57 +1,58 @@
+function dayName(today) {
+   var weekdays = [
+      "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
+   var day = weekdays[today];
+   return day;
+}
+
+var thead = document.querySelector("table");
+var tbody = document.querySelector("table");
+
 var forecastRequest = new XMLHttpRequest();
-var apiString = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=b6dcbfd399dcaf20477af22ae69192c2';
-forecastRequest.open('Get', apiURLstring, true);
+var apiString1 = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=b6dcbfd399dcaf20477af22ae69192c2';
+forecastRequest.open('Get', apiURLstring1, true);
 forecastRequest.send();
 
 forecastRequest.onload = function() {
     var weatherData = JSON.parse(forecastRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('day1').innerHTML = weatherData.list[7].main.temp;
-    document.getElementById('day2').innerHTML = weatherData.list[14].main.temp;
-    document.getElementById('day3').innerHTML = weatherData.list[22].main.temp;
-    document.getElementById('day4').innerHTML = weatherData.list[31].main.temp;
-    document.getElementById('day5').innerHTML = weatherData.list[39].main.temp;
+    var date = new Date();
+    var today = date.getDay();
 
-   for (var i = 0; i < weatherData.length; i++){
-        if(weatherData.list[i].dt_txt.search('18:00:00') != -1){
-           document.getElementById('day1').innerHTML = weatherData.list[i].main.temp;
-        }
+    var list = weatherData.list;
+    var row1 = document.createElement("tr");
+    var row2 = document.createElement("tr");
 
-     var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[7].weather[0].icon + ".png";
-     var desc = weatherData.list[7].weather[0].description;
-     document.getElementById('weatherimage1').scroll = icon;
-            
-     document.getElementById('weatherimage1').setAttribute('src', icon);
-     document.getElementById('weatherimage1').setAttribute('alt', desc);
-   
-
-    var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[15].weather[0].icon + ".png";
-    var desc = weatherData.list[15].weather[0].description;
-    document.getElementById('weatherimage2').scroll = icon;
+    for (i = 0; i < list.length; i++) {
+       var theader = document.createElement("th");
+       var tdata1 = document.createElement("td");
+       //var tdata2 = document.createElement("td");
+       var img = document.createElement("img");
     
-    document.getElementById('weatherimage2').setAttribute('src', icon);
-    document.getElementById('weatherimage2').setAttribute('alt', desc);
+        if(list[i].dt_txt.includes("18:00:00")) {
+           console.log("gotcha");
 
-    var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[23].weather[0].icon + ".png";
-    var desc = weatherData.list[23].weather[0].description;
-    document.getElementById('weatherimage3').scroll = icon;
-    
-    document.getElementById('weatherimage3').setAttribute('src', icon);
-    document.getElementById('weatherimage3').setAttribute('alt', desc);
+           if(today == 7) {
+              today = 0;
+           }
+           var dayname = dayName(today);
+           today++;
 
-    var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[31].weather[0].icon + ".png";
-    var desc = weatherData.list[31].weather[0].description;
-    document.getElementById('weatherimage4').scroll = icon;
-    
-    document.getElementById('weatherimage4').setAttribute('src', icon);
-    document.getElementById('weatherimage4').setAttribute('alt', desc);
+           var temp = list[i].main.temp;
+           var sun = "https://openweathermap.org/img/w/" + list[i].weather[0].icon + ".png";
+           var description = list[i].weather[0].description;
 
-    var icon = 'https://openweathermap.org/img/wn/' + weatherData.list[39].weather[0].icon + ".png";
-    var desc = weatherData.list[39].weather[0].description;
-    document.getElementById('weatherimage5').scroll = icon;
-    
-    document.getElementById('weatherimage5').setAttribute('src', icon);
-    document.getElementById('weatherimage5').setAttribute('alt', desc);
-}
-}
+           thead.textContent = dayname;
+           img.setAttribute("src", sun);
+           img.setAttribute("alt", description);
+           tdata1.appendChild(img);
+           tdata1.textContent = temp + "&#8457";
+
+           row1.appendChild(theader);
+           row2.appendChild(tdata1);
+         }
+         thead.appendChild(row1);
+         tbody.appendChild(row2)
+      }
+   };

19b4cf3eb64d51f2f3ff94e66a9afea1f7021034
marmii <miriam26@byui.edu>
2019-07-03 08:37:00 -0600

    round

--- assignments/lesson-11/js/weatherapi.js
+++ assignments/lesson-11/js/weatherapi.js
@@ -1,22 +1,23 @@
+//gets the next 5 days of the week
 function dayName(today) {
    var weekdays = [
       "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
    var day = weekdays[today];
    return day;
 }
-
+//finds the table tag in the document
 var thead = document.querySelector("table");
 var tbody = document.querySelector("table");
-
+//grabs the api information and sends it to the function
 var forecastRequest = new XMLHttpRequest();
 var apiURLString1 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=b6dcbfd399dcaf20477af22ae69192c2";
 forecastRequest.open("Get", apiURLString1, true);
 forecastRequest.send();
-
+//tells the browser to run the function onloading
 forecastRequest.onload = function() {
     var weatherData = JSON.parse(forecastRequest.responseText);
     console.log(weatherData);
-
+//creates variables to hold the information going into the table
     var date = new Date();
     var today = date.getDay();
 
@@ -24,7 +25,8 @@ forecastRequest.onload = function() {
     var row1 = document.createElement("tr");
     var row2 = document.createElement("tr");
     var row3 = document.createElement("tr");
-
+//runs a for loop to find the information listed in the 18:00 spot of the JSON
+//also creates new elements to hold that information
     for (i = 0; i < list.length; i++) {
        var theader = document.createElement("th");
        var tdata1 = document.createElement("td");
@@ -33,27 +35,31 @@ forecastRequest.onload = function() {
     
         if(list[i].dt_txt.includes("18:00:00")) {
            console.log("gotcha");
-
+//Starts the week over if it's Sunday
            if(today == 7) {
               today = 0;
            }
+//puts the name of the day in place of the number
            var dayname = dayName(today);
            today++;
-
+//grabs the temp, icon, and description from the list and stores them in var temp, sun, and description
            var temp = list[i].main.temp;
            var sun = "https://openweathermap.org/img/w/" + list[i].weather[0].icon + ".png";
            var description = list[i].weather[0].description;
-
+//Applies the day name as text, gives an alt tag and src tag to icon
+//adds image to table as well as temp
            theader.textContent = dayname;
            img.setAttribute("src", sun);
            img.setAttribute("alt", description);
            tdata1.appendChild(img);
+           Math.round(temp);
            tdata2.textContent = temp + "\xB0F";
-
+//makes it a 3 row table
            row1.appendChild(theader);
            row2.appendChild(tdata1);
            row3.appendChild(tdata2);
          }
+//
          thead.appendChild(row1);
          tbody.appendChild(row2);
          tbody.appendChild(row3);

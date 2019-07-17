0f0dbabad141cac74ad1160b13d87fca6bf40852
marmii <miriam26@byui.edu>
2019-07-17 17:39:22 -0600

    links

---
+++ assignments/final/js/currentdate.js
@@ -0,0 +1,20 @@
+function currentDate() {
+    var mydate = new Date();
+    var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var weekday = dayarray[mydate.getDay()];
+    var day = mydate.getDate();
+    var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    var month = montharray[mydate.getMonth()];
+    
+    var year = mydate.getYear();    
+        if(year < 1000){
+            year +=1900
+            }
+    var fulldate = weekday + ", " + month + " " + day + " " + year;
+    
+   
+    document.getElementById("today").innerHTML = fulldate;
+    
+}
+currentDate();
+

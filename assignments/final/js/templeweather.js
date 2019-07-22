727819df2f4b4269b8bf0773d246c65d838e92a8
marmii <miriam26@byui.edu>
2019-07-22 16:41:09 -0600

    temp

--- assignments/final/js/templeweather.js
+++ assignments/final/js/templeweather.js
@@ -7,12 +7,31 @@ weatherRequest.onload = function() {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
 
-    document.getElementById('temp').innerHTML = weatherData.list[0].main.temp;
-    document.getElementById('current').innerHTML = weatherData.list[0].weather[0].description;
+    document.getElementById('btemp').innerHTML = weatherData.list[0].main.temp;
+    document.getElementById('bcurrent').innerHTML = weatherData.list[0].weather[0].description;
 
-    /*Input: Get temp and windspeed*/
-let t = parseInt(document.getElementById('temp').innerHTML);
-let s = parseInt(document.getElementById('current').innerHTML);
+    document.getElementById('ltemp').innerHTML = weatherData.list[0].main.temp;
+    document.getElementById('lcurrent').innerHTML = weatherData.list[0].weather[0].description;
+
+    document.getElementById('stemp').innerHTML = weatherData.list[0].main.temp;
+    document.getElementById('scurrent').innerHTML = weatherData.list[0].weather[0].description;
+
+    document.getElementById('otemp').innerHTML = weatherData.list[0].main.temp;
+    document.getElementById('ocurrent').innerHTML = weatherData.list[0].weather[0].description;
+
+    /*Input: Get temp and current Conditions*/
+    /*
+parseInt(document.getElementById('temp').innerHTML);
+parseInt(document.getElementById('current').innerHTML);
+
+parseInt(document.getElementById('temp').innerHTML);
+parseInt(document.getElementById('current').innerHTML);
+
+parseInt(document.getElementById('temp').innerHTML);
+parseInt(document.getElementById('current').innerHTML);
+
+parseInt(document.getElementById('temp').innerHTML);
+parseInt(document.getElementById('current').innerHTML);
 
 /*for (var i= 0; i< weatherData.list.length; i++){
     if (weatherData.list.dt_txt.includes = "18:00:00"){

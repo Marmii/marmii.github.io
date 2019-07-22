448e38771ce3d9f781e4cf7173ddda739f70f2ee
marmii <miriam26@byui.edu>
2019-07-22 15:06:32 -0600

    commit

--- assignments/final/js/templespage.js
+++ assignments/final/js/templespage.js
@@ -1,5 +1,5 @@
-var header = document.querySelector("header");
-var section = document.querySelector("section");
+var figure = document.querySelector("figure");
+//var section = document.querySelector("section");
 
 var requestURL = "https://marmii.github.io/assignments/final/temple-data.json";
 
